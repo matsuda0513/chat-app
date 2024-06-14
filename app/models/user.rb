95304319322963dc4d3d6e65f1770da37bdb7ae6
matsuda0513 <gdluk4everythinguact@gmail.com>
2024-06-14 17:08:42 +0900

    各モデルのテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable  
+        #  validatableというdeviseのモジュールが含まれており、これによりemailの形式と一意性、パスワードの長さ（6~128文字）と形式（半角英数字）についてのバリデーションが自動的に設定されます
   
   has_many :room_users
   has_many :rooms, through: :room_users
